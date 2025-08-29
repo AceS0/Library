c4af3904818bea8ce655b883c034fa3041e391e0
AceS0 <abdulcelilsekerci@gmail.com>
2025-08-29 08:34:48 +0200

    Step 5. Implement the REST API - WorkController: DONE

--- src/main/java/dk/teqs/library/catalog/controller/WorkController.java
+++ src/main/java/dk/teqs/library/catalog/controller/WorkController.java
@@ -2,11 +2,10 @@ package dk.teqs.library.catalog.controller;
 
 import dk.teqs.library.catalog.model.Work;
 import dk.teqs.library.catalog.service.WorkService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,36 @@ public class WorkController {
     @GetMapping
     public ResponseEntity<List<Work>> getAllWorks() {
         List<Work> works = workService.getAllWorks();
+        return ResponseEntity.ok(works);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Work> getWorkById(@PathVariable Long id){
+        Work work = workService.getWorkById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(work);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Work> addWork(@RequestBody Work work){
+        Work newWork = workService.createWork(work);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newWork);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Work> updateWork(@PathVariable Long id, @RequestBody Work work){
+        Work newWork = workService.updateWork(id,work);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWork(@PathVariable Long id){
+        workService.deleteWork(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Work>> searchWork(@RequestParam(required = false) String title){
+        List<Work> works = workService.searchWorks(title);
         return ResponseEntity.ok(works);
     }
 }
